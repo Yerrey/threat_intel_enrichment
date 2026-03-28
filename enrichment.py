5c91e7e1c93835e432d482878985022cb0aa2191
Rey Maldonado <rey.maldonado357@gmail.com>
2026-03-28 11:13:59 -0500

    Refactor script into modular functions

--- enrichment.py
+++ enrichment.py
@@ -15,8 +15,6 @@ if api_key is  None:
     print("ERROR: NO API KEY FOUND")
     exit()
 
-
-
 indicators = [
     {"type": "ip", "value": "8.8.8.8"},
     {"type": "domain", "value": "google.com"},
@@ -26,52 +24,51 @@ indicators = [
     {"type": "domain", "value": "guitarcenter.com"}
 ]
 
+def build_url(indicator):
+    if indicator["type"] == "ip":
+        url = f"https://www.virustotal.com/api/v3/ip_addresses/{indicator['value']}"
+    elif indicator["type"] == "domain":
+        url = f"https://www.virustotal.com/api/v3/domains/{indicator['value']}"
 
-with open("triage_report.csv", "w", newline='', encoding='utf-8') as file:
-    
-    writer = csv.writer(file)
-    writer.writerow(["IP | DOMAIN", "Malicious", "Suspicious", "Harmless", "Verdict"])
+    return url
 
+def query_virustotal(url, api_key):
+    response = requests.get(url, headers = {"x-apikey": api_key})
+    result = response.json()
+    return result
 
+def get_verdict(malicious):
+    if malicious > 5:
+        return "⚠️ HIGH RISK"
+    if 1 <= malicious <= 5:
+        return "⚠️ SUSPICIOUS" 
+    if malicious == 0:
+        return "✅ CLEAN"
+        
 
-    for indicator in indicators:
+def run_analysis(indicators):
 
-        if indicator["type"] == "ip":
-            url = f"https://www.virustotal.com/api/v3/ip_addresses/{indicator['value']}"
-        elif indicator["type"] == "domain":
-            url = f"https://www.virustotal.com/api/v3/domains/{indicator['value']}"
+    with open("triage_report.csv", "w", newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(["IP | DOMAIN", "Malicious", "Suspicious", "Harmless", "Verdict"])
 
-        response = requests.get(url, headers = {"x-apikey": api_key})
-        result = response.json()
+        for indicator in indicators: 
+            url = build_url(indicator)
+            result = query_virustotal(url,api_key)
+
+            malicious = result["data"]["attributes"]["last_analysis_stats"]["malicious"]
+            suspicious = result["data"]["attributes"]["last_analysis_stats"]["suspicious"]
+            harmless = result["data"]["attributes"]["last_analysis_stats"]["harmless"]
+
+            verdict = get_verdict(malicious)
             
-
-
-        
-
-
-        malicious = result["data"]["attributes"]["last_analysis_stats"]["malicious"]
-        suspicious = result["data"]["attributes"]["last_analysis_stats"]["suspicious"]
-        harmless = result["data"]["attributes"]["last_analysis_stats"]["harmless"]
-
-        
-        print(f"IP: {indicator['value']} | Malicious: {malicious} | Suspicious: {suspicious} | Harmless: {harmless}")
-        if malicious > 5:
-            verdict = "⚠️ HIGH RISK"
+            print(f"IP/DOMAIN: {indicator['value']} | Malicious: {malicious} | Suspicious: {suspicious} | Harmless: {harmless}")
             print(verdict)
-        elif 1 <= malicious <= 5:
-            verdict = "⚠️ SUSPICIOUS"
-            print(verdict)
-        elif malicious == 0:
-            verdict = "✅ CLEAN"
-            print(verdict)
+            writer.writerow([indicator["value"], malicious, suspicious, harmless, verdict])
+            # using rate limits to stay within VirusTotal's 4 requests per minute
+            time.sleep(15)
 
-        writer.writerow([indicator["value"], malicious, suspicious, harmless, verdict])
-        # using rate limits to stay within VirusTotal's 4 requests per minute
-        time.sleep(15)
-
-
-
-
+run_analysis(indicators)
 
 
 
